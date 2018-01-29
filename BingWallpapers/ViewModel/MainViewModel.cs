36f4d6a49fcae4090c30f4cb18fc2c468c1403e9
Grant Howard <the1812@outlook.com>
2018-01-29 14:20:12 +0800

    Color auto change.

--- BingWallpapers/ViewModel/MainViewModel.cs
+++ BingWallpapers/ViewModel/MainViewModel.cs
@@ -5,12 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using BingWallpapers.Languages;
 using BingWallpapers.View;
+using Ace.Wpf;
+using System.Windows.Media;
+using System.Windows;
 
 namespace BingWallpapers.ViewModel
 {
     sealed partial class MainViewModel : ViewModel<MainView, MainLanguage>
     {
-        public MainViewModel(MainView view) : base(view, new MainLanguage()) { }
+        public MainViewModel(MainView view) : base(view, new MainLanguage())
+        {
+            view.OnColorizationChanged(color =>
+            {
+                Application.Current.Resources["DwmColor"] = new SolidColorBrush(color);
+                Application.Current.Resources["TitleBarColor"] = new SolidColorBrush(DwmEffect.WindowTitleColor);
+            });
+        }
 
     }
 }
